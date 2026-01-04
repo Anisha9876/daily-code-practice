53adcf71af7713ea0cdf093414074415131b3e86
Anisha9876 <routrayanisha30@gmail.com>
2026-01-04 12:31:42 +0530

    Sum of digits

---
+++ Day2/SumOfDigits.java
@@ -0,0 +1,19 @@
+package Day2;
+
+import java.util.Scanner;
+
+public class SumOfDigits {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        System.out.println("Enter a number");
+        int num=sc.nextInt();
+        int rem;
+        int sum=0;
+        while(num>0){
+            rem=num%10;
+            sum=sum+rem;
+            num=num/10;
+        }
+        System.out.println("sum of digits "+ sum);
+    }
+}
