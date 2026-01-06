0977b4226d07609d046cb8e3c2db7bca102fe638
Anisha9876 <routrayanisha30@gmail.com>
2026-01-06 09:30:06 +0530

    Reverse a triangle

---
+++ Day_3/ReverseTriPtrn.java
@@ -0,0 +1,19 @@
+package Day_3;
+
+public class ReverseTriPtrn {
+    public static void main(String[] args) {
+//        * * * * *
+//        * * * *
+//        * * *
+//        * *
+//        *
+
+        for(int i=0;i<5;i++){
+            for(int j=5-i;j>0;j--){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+
+    }
+}
