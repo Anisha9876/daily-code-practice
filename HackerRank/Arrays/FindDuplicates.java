c0d643bcc7d919ac07e783742ff4757aa0e9f59c
Anisha9876 <routrayanisha30@gmail.com>
2026-03-10 16:53:58 +0530

    Find Duplicates in an Array

---
+++ HackerRank/Arrays/FindDuplicates.java
@@ -0,0 +1,19 @@
+package HackerRank.Arrays;
+
+import java.util.HashMap;
+
+public class FindDuplicates {
+    public static void main(String[] args) {
+        int[] arr={1,2,3,1,2,5,6};
+        HashMap<Integer,Integer> map=new HashMap<>();
+        for(int i=0;i<arr.length;i++){
+            map.put(arr[i],map.getOrDefault(arr[i],0)+1);
+        }
+        for(int n:map.keySet()){
+            if(map.get(n)>1){
+                System.out.print(n+" ");
+            }
+
+        }
+    }
+}
