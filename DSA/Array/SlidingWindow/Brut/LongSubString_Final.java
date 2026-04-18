8a40329ee91c2e85ac3d9e23beaba6d27d6a2500
Anisha9876 <routrayanisha30@gmail.com>
2026-04-18 11:18:00 +0530

    Find the longest Substring nonRepeatative using HashSet

--- DSA/Array/SlidingWindow/Brut/LongSubString_Final.java
+++ DSA/Array/SlidingWindow/Brut/LongSubString_Final.java
@@ -1,4 +1,33 @@
 package DSA.Array.SlidingWindow.Brut;
 
+import java.util.HashSet;
+
 public class LongSubString_Final {
+    public static void main(String[] args) {
+        //Longest Substring which contains non-repeating value
+        String str="pwwkew";
+        String longStr="";
+        int maxLen=0;
+        HashSet<Character> set=new HashSet<>();
+        int i=0;
+        int left=0,right=0;
+        while(right<str.length()){
+            char ch=str.charAt(right);
+            if(!set.contains(ch)){
+                set.add(ch);
+                if (right-left+1 >maxLen ) {
+                    maxLen=right-left+1;
+                    longStr = str.substring(left,right+1);
+                }
+                right++;
+
+            }
+            else {
+                set.remove(str.charAt(left));
+                left++;
+            }
+        }
+        System.out.println(longStr);
+
+    }
 }
