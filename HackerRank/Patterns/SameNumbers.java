fe2135ea682f0726e8eea01235e6d8ccac7c09bd
Anisha9876 <routrayanisha30@gmail.com>
2026-04-23 09:56:44 +0530

    Same numbers in rectangular shape

---
+++ HackerRank/Patterns/SameNumbers.java
@@ -0,0 +1,15 @@
+package HackerRank.Patterns;
+
+public class SameNumbers {
+    public static void main(String[] args) {
+//        1 1 1
+//        2 2 2
+//        3 3 3
+        for(int i=0;i<3;i++){
+            for(int j=0;j<3;j++){
+                System.out.print((i+1)+" ");
+            }
+            System.out.println();
+        }
+    }
+}
