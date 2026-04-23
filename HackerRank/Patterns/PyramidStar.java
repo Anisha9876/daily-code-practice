6fff114e911d16d383c18728870f3d385249ff94
Anisha9876 <routrayanisha30@gmail.com>
2026-04-23 10:14:13 +0530

    Pyramid

---
+++ HackerRank/Patterns/PyramidStar.java
@@ -0,0 +1,21 @@
+package HackerRank.Patterns;
+
+public class PyramidStar {
+    public static void main(String[] args) {
+//           *
+//          ***
+//         *****
+//        *******
+        for(int i=0;i<=4;i++){
+            for(int j=4;j>=i;j--){
+                System.out.print(" ");
+            }
+            for(int j=0;j<(2*i+1);j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+
+    }
+}
