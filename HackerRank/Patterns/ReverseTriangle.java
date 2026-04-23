b1a2325ecc199615a7e3f290417849e1bcd5213c
Anisha9876 <routrayanisha30@gmail.com>
2026-04-23 10:02:09 +0530

    Reverse a triangle

---
+++ HackerRank/Patterns/ReverseTriangle.java
@@ -0,0 +1,18 @@
+package HackerRank.Patterns;
+
+public class ReverseTriangle {
+    public static void main(String[] args) {
+//* * * *
+//* * *
+//* *
+//*
+
+        for(int i=0;i<4;i++){
+            for(int j=3;j>=i;j--){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+
+    }
+}
