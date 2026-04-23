cb9d43f638caddf4c489229816825783a224db98
Anisha9876 <routrayanisha30@gmail.com>
2026-04-23 09:51:40 +0530

    Triangle

---
+++ HackerRank/Patterns/Triangle_Right.java
@@ -0,0 +1,19 @@
+package HackerRank.Patterns;
+
+public class Triangle_Right {
+    public static void main(String[] args) {
+//        *
+//        * *
+//        * * *
+//        * * * *
+        for(int i=0;i<4;i++){
+            for(int j=0;j<=i;j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+
+
+
+    }
+}
