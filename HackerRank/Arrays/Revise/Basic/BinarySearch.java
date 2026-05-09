ac541e787de8e6b5810b22bba767e3eeb82d8dfc
Anisha9876 <routrayanisha30@gmail.com>
2026-05-09 18:50:38 +0530

    Finding index of the taregeted element

---
+++ HackerRank/Arrays/Revise/Basic/BinarySearch.java
@@ -0,0 +1,15 @@
+package HackerRank.Arrays.Revise.Basic;
+
+public class BinarySearch {
+    public static void main(String[] args) {
+        int[] arr={1,5,3,7,8,9};
+        int target=18;
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]==target){
+                System.out.println(i);
+                return;
+            }
+        }
+        System.out.println(target+ " target is not present in this array");
+    }
+}
